de489ec6d6a56085af3b255e27935b8f5dacd6dc
kaczka <manfil28@gmail.com>
2024-02-03 17:36:31 +0100

    init

---
+++ src/main/java/org/example/prawo_jazdy/PrawoJazdyApplication.java
@@ -0,0 +1,13 @@
+package org.example.prawo_jazdy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrawoJazdyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrawoJazdyApplication.class, args);
+    }
+
+}
